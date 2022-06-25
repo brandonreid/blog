22dde269a9586fa5717308466ef788016c66f5c8
Brandon Reid <brandonreid@Brandons-MBP.lan>
2022-06-25 09:07:09 -0700

    make base route render posts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,36 @@
 <script context="module">
-	export async function load() {
+	export const load = async ({ fetch }) => {
+		const postRes = await fetch(`/api/posts.json`)
+		const { posts } = await postRes.json()
+
+    const totalRes = await fetch(`/api/posts/count.json`)
+    const { total } = await totalRes.json()
+
 		return {
-			status: 302,
-			redirect: "/blog"
-		};
+			props: { posts, total }
+		}
 	}
 </script>
+
+
+<script>
+  import PostsList from '$lib/components/PostsList.svelte'
+  import Pagination from '$lib/components/Pagination.svelte'
+	import { siteDescription } from '$lib/config'
+	export let posts
+  export let total
+</script>
+
+
+<svelte:head>
+	<title>Blog</title>
+	<meta data-key="description" name="description" content={siteDescription}>
+</svelte:head>
+
+<div class="container">
+	<h1>Blog</h1>
+
+	<PostsList {posts} />
+
+	<Pagination currentPage={1} totalPosts={total} />
+</div>

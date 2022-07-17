75086b7a54c4404784272595b054fc7515a1bbc1
brandonreid <brandonreidak@gmail.com>
2022-07-17 15:39:40 +0000

    Deploying to gh-pages from @ brandonreid/blog@fc851223424fb182486da34ec232e20461090027 🚀

---
+++ _app/chunks/formatDate-fc285d6f.js
@@ -0,0 +1,1 @@
+function e(n){return new Date(n.year,n.month-1,n.day)}function t(n){return new Date(n.year,n.month-1,n.day).toLocaleDateString("en-us",{year:"numeric",month:"long",day:"numeric"})}export{t as f,e as g};
